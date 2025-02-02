122737865c14ca9bceaf7e8892967b964c306730
Joseph Lee <josephlee498@gmail.com>
2025-02-02 19:43:24 +0000

    display the add movie form on the admin dashboard at the admin's request

--- src/app/admindashboard/admindashboard.component.ts
+++ src/app/admindashboard/admindashboard.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admindashboard',
   standalone: false,
-  
+
   templateUrl: './admindashboard.component.html',
-  styleUrl: './admindashboard.component.css'
+  styleUrl: './admindashboard.component.css',
 })
 export class AdmindashboardComponent {
+  showAddMovieForm: boolean = false;
 
+  toggleAddMovieForm(): void {
+    this.showAddMovieForm = !this.showAddMovieForm;
+  }
 }
